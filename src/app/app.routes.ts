e295d1cc61dfbaf171994617959896822692884c
Alejandra <hildarosasb@hotmail.com>
2025-11-17 23:31:17 -0500

    Rutas implementadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,40 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
+import { ConocenosComponent } from './conocenos/conocenos.component';
+import { ProductosComponent } from './productos/productos.component';
+import { LoginComponent } from './login/login.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { BoletaComponent } from './boleta/boleta.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+        path: '',
+        component: InicioComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+ 
+    {
+        path: 'productos',
+        component: ProductosComponent
+    },
+    {
+        path: 'carrito',
+        component: CarritoComponent
+    },
+    {
+        path: 'boleta',
+        component: BoletaComponent
+    },
+    {
+        path: 'conocenos',
+        component: ConocenosComponent
+    },
+  
+ 
+    {
+        path: '**', // Ruta 404 - DEBE IR AL FINAL
+        component: NotFoundComponent
+    }];
